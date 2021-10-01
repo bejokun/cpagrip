0653990a41651e7d79b26eb6b4517223e60eb1b8
bejokun <bejokun34@gmail.com>
2021-10-01 14:57:40 +0700

    upload

---
+++ popku.js
@@ -0,0 +1,8 @@
+<div class="exit-intent-popup">
+	<div class="newsletter">
+		<center><b>⏱ ⏱ ⏱ LIMITED OFFER 🔥 🔥 🔥</b>
+		<p>💰 💰 💰 Get a $200++ Mastercard Gift Card! 👇👇👇</p>
+		<a href="https://fasterfiles.net/show.php?l=0&u=33789&id=36874&tracking_id=" rel="noopener noreferrer nofollow" target=" _blank"><img alt="master crad" src="https://i.imgur.com/k8ILCRN.png"></a></center>
+		<span class="close">X</span>
+	</div>
+</div>
